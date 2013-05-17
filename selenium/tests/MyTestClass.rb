a8eebaf6b774c80ddc03bf0f1262a4c433b5c4fd
Rohini <rohini@rohini-desktop.(none)>
2013-05-17 16:27:41 +0530

    fixes for username issue in testcase

--- selenium/tests/MyTestClass.rb
+++ selenium/tests/MyTestClass.rb
@@ -13,9 +13,9 @@ class SeleniumTestBase < Test::Unit::TestCase
     #Some code which will query a DB for you
   end
   def start_browser(browserType)
-    self.username = ENV['SAUCE_USERNAME']
-    self.key = ENV['SAUCE_ACCESS_KEY']
-    hub_url = "%s:%s@ondemand.saucelabs.com:80" % [self.username, self.key]
+    username = ENV['SAUCE_USERNAME']
+    key = ENV['SAUCE_ACCESS_KEY']
+    hub_url = "%s:%s@ondemand.saucelabs.com:80" % [username, key]
     return @myDriver = @myDriver.for(:remote, :url => hub_url)
     #Some code which will launch different types of browsers
   end
