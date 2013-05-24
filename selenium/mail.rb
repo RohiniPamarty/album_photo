34a111d80fe1253f17a364211e22311107064f81
Rohini <rohini@rohini-desktop.(none)>
2013-05-24 17:21:18 +0530

    with pony

--- selenium/mail.rb
+++ selenium/mail.rb
@@ -1,17 +1,2 @@
-require 'net/smtp'
-def send_mail
-puts "sending..."
-message = <<MESSAGE_END
-From: Private Person <rohini@neevtech.com>
-To: A Test User <rohini@neevtech.com>
-Subject: SMTP e-mail test
-
-This is a test e-mail message.
-MESSAGE_END
-
-Net::SMTP.start('localhost') do |smtp|
-  smtp.send_message message, 'rohini@neevtech.com', 
-                             'rohini@neevtech.com'
-end
-end
-send_mail
+require 'pony'
+Pony.mail(:to => 'rohini@neevtech.com', :from => 'rohini@neevtech.com', :subject => "hi!", :body => "hello",:via => :sendmail)
