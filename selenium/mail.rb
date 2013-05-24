903e56f1832dc0a81436c9ed8fef98e16320bff4
Rohini <rohini@rohini-desktop.(none)>
2013-05-24 16:35:52 +0530

    again

--- selenium/mail.rb
+++ selenium/mail.rb
@@ -1,11 +1,17 @@
-require 'rubygems'
-require 'pony'
-puts 'sending mail...'
-Pony.mail :to => 'rohini@neevtech.com',
-          :from => 'rohini@neevtech.com',
-          :subject => 'here we get near yeah yeah!',
-          :body => 'wsup!'
-puts 'send mail...'
+require 'net/smtp'
+def send_mail
+puts "sending..."
+message = <<MESSAGE_END
+From: Private Person <rohini@neevtech.com>
+To: A Test User <rohini@neevtech.com>
+Subject: SMTP e-mail test
 
+This is a test e-mail message.
+MESSAGE_END
 
-
+Net::SMTP.start('localhost') do |smtp|
+  smtp.send_message message, 'rohini@neevtech.com', 
+                             'rohini@neevtech.com'
+end
+end
+send_mail
