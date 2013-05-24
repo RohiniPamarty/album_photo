2af7af770845eead08f8874fcdcc4ddffc57725c
Rohini <rohini@rohini-desktop.(none)>
2013-05-24 15:16:37 +0530

    changes for mails

--- selenium/mail.rb
+++ selenium/mail.rb
@@ -1,14 +1,11 @@
-require 'net/smtp'
-
-message = <<MESSAGE_END
-From: Private Person <rohini@neevtech.com>
-To: A Test User <rohini@neevtech.com>
-Subject: SMTP e-mail test
-
-This is a test e-mail message.
-MESSAGE_END
-
-Net::SMTP.start('localhost') do |smtp|
-  smtp.send_message message, 'rohini@neevtech.com', 
-                             'rohini@neevtech.com'
+require 'mail'
+mail = Mail.new do
+  from     'rohini@neevtech.com'
+  to       'rohini@neevtech.com'
+  subject  'Here is the image you wanted'
+  body     'This is test'
 end
+
+mail.delivery_method :sendmail
+
+mail.deliver
