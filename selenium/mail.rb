e3f2de91a7295c0bef9f93ab371f72338656d42c
Rohini <rohini@rohini-desktop.(none)>
2013-05-24 14:20:22 +0530

    changes for email

--- selenium/mail.rb
+++ selenium/mail.rb
@@ -1,2 +1,14 @@
-require 'pony'
-Pony.mail(:to => 'rohini@neevtech.com', :from => 'rohini@neevtech.com', :subject => "hi!", :body => "hello",:via => :sendmail)
+require 'net/smtp'
+
+message = <<MESSAGE_END
+From: Private Person <rohini@neevtech.com>
+To: A Test User <rohini@neevtech.com>
+Subject: SMTP e-mail test
+
+This is a test e-mail message.
+MESSAGE_END
+
+Net::SMTP.start('localhost') do |smtp|
+  smtp.send_message message, 'rohini@neevtech.com', 
+                             'rohini@neevtech.com'
+end
