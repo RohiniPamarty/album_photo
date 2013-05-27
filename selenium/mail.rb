13bbec200171a0a55299a2e2e2ffc1cccd6dd681
Rohini <rohini@rohini-desktop.(none)>
2013-05-27 11:26:42 +0530

    change for mails again

--- selenium/mail.rb
+++ selenium/mail.rb
@@ -1,2 +1,4 @@
-require 'pony'
-Pony.mail(:to => 'rohini@neevtech.com', :from => 'rohini@neevtech.com', :subject => "hi!", :body => "hello",:via => :sendmail)
+out = require 'pony'
+puts "#{out}"
+ouput = Pony.mail(:to => 'rohini@neevtech.com', :from => 'rohini@neevtech.com', :subject => "hi!", :body => "hello",:via => :sendmail)
+puts "#{ouput}"
