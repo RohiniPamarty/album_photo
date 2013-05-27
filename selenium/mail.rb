6cabc338c3a1bb4d0149a763bec88cbcefbe282a
Rohini <rohini@rohini-desktop.(none)>
2013-05-27 11:42:52 +0530

    change for smtp

--- selenium/mail.rb
+++ selenium/mail.rb
@@ -1,4 +1,17 @@
-out = require 'pony'
-puts "#{out}"
-ouput = Pony.mail(:to => 'rohini@neevtech.com', :from => 'rohini@neevtech.com', :subject => "hi!", :body => "hello",:via => :sendmail)
-puts "#{ouput}"
+
+
+require 'net/smtp'
+
+message = <<MESSAGE_END
+From: Private Person <rohini@neevtech.com>
+To: A Test User <rohini@neevtech.com>
+Subject: SMTP e-mail test
+
+This is a test e-mail message.
+MESSAGE_END
+
+output1 = Net::SMTP.start('localhost') do |smtp|
+  smtp.send_message message, 'rohini@neevtech.com', 
+                             'rohini@neevtech.com'
+end
+puts "#{output1}"
