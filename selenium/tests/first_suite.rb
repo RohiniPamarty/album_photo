a033c54fd3686356360f68cf080d3fffedbe83b9
Rohini <rohini@rohini-desktop.(none)>
2013-05-28 16:34:08 +0530

    test_suites

---
+++ selenium/tests/first_suite.rb
@@ -0,0 +1,17 @@
+#relevant test::unit requires
+require 'test/unit'
+require 'test/unit/ui/console/testrunner'
+
+#TestCase classes that contain the methods I want to execute
+require 'MyTestClass'
+
+#create a new empty TestSuite, giving it a name
+my_tests = Test::Unit::TestSuite.new("My Special Tests")
+
+#add the test method called 'test_one' defined
+#in the TestOne TestCase class to the my_tests
+#test suite defined above
+my_tests << MyTestClass.new('test2')
+
+#run the suite
+Test::Unit::UI::Console::TestRunner.run(my_tests)
