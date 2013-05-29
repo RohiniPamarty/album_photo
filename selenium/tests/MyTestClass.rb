17c049b14be7b2f73c135b6d7c78286c7b23e721
Rohini <rohini@rohini-desktop.(none)>
2013-05-29 12:06:06 +0530

    changes for suites

--- selenium/tests/MyTestClass.rb
+++ selenium/tests/MyTestClass.rb
@@ -109,6 +109,6 @@ class MyTestClass < Test::Unit::TestCase
 #  end
   def test2
     @instance_failure = LoginTests.new(Selenium::WebDriver)
-    assert(@instance_failure.unsuccessful_login_test)
+    assert(@instance_failure.unsuccessful_login_test, "This is expected to be true")
   end
 end
